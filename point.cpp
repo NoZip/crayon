7957d7f26bcb5f8cfcfcc8e9c298d41a33e9ab51
CamilleMaurice <camillemaurice@gmail.com>
2013-04-10 13:48:03 +0300

    Update point.cpp
    polar point constructor added

--- point.cpp
+++ point.cpp
@@ -10,12 +10,25 @@ Point::Point(float x, float y){
   _x = x; _y =y;
 }
 
+Point::Point(Angle theta, double r) {
+ m_x = x_from_polar(theta, r);
+ m_y = y_from_polar(theta,r);
+}
+
 float Point::get_x() const{
   return _x;
 }
 
 float Point::get_y() const{
   return _y;
+}
+
+float Point::x_from_polar(Angle theta, double r) {
+   return r * cos(theta);
+}
+
+float Point::y_from_polar(Angle theta, double r) {
+   return r * cos(theta);
 }
 
 Point::~Point(){}
