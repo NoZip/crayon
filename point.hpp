e5b49e8792fd595254258b3020d56586586b5f04
CamilleMaurice <camillemaurice@gmail.com>
2013-04-10 13:41:30 +0300

    Update point.hpp

--- point.hpp
+++ point.hpp
@@ -5,12 +5,17 @@ class Point{
  public:
   Point();
   Point(float x, float y);
+  Point(Angle theta, double r);
   ~Point();
   float get_x() const;
   float get_y() const;
  private:
   float _x;
   float _y;
+  float x_from_polar(Angle theta, double r);
+  float y_from_polar(Angle theta, double r);
 };
+
+Point operator+(Point p1, Point p2);
 
 #endif
